68dbc78230580617f153debea06b2ab18d2a69f6
Aaron Lewis <aaronglewis@outlook.com>
2026-06-09 11:01:51 +0100

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Aaron Lewis - Senior Backend & Platform Engineer",
-  description: "Senior backend-focused software engineer with extensive experience designing and delivering secure, cloud-native financial and eligibility platforms in highly regulated environments.",
+  title: "Aaron Lewis - Software Engineer",
+  description: "Backend-focused software engineer with extensive experience designing and delivering secure, cloud-native financial and eligibility platforms in highly regulated environments.",
 };
 
 export default function RootLayout({
